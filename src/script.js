244ccdf84ca48dfb1782b8b3a4a42da06e891b84
RoyaAlmasimehr <almasimehrroya@gmail.com>
2023-10-31 14:06:35 +0330

    Added relevant icon from the API

--- src/script.js
+++ src/script.js
@@ -17,12 +17,20 @@ date.innerHTML = ` Last Updated:${day}
 ${hours}: ${minutes} `;
 
 function displayTemperature(response) {
-  let temperature = Math.round(response.data.main.temp);
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -30,30 +38,9 @@ function displayTemperature(response) {
   wind.innerHTML = `Wind:${Math.round(response.data.wind.speed)} km/h`;
 }
 
-  
-  //   document.querySelector("#temperature-description").innerHTML =
-
-  //   temperatureElement.innerHTML = `${temperature}°C`;
-
-  //   document.querySelector("#city").innerhtml = response.data.name;
-
-  //   let temperature = Math.round(response.data.main.temp);
-  //   let h1 = document.querySelector("h1");
-  //   h1.innerHTML = `${response.data.name}`;
-  //   console.log(temperature);
-  //   let temperatureElement = document.querySelector("#temperature");
-  //   temperatureElement.innerHTML = `${temperature}°C`;
-  //   let humidity = document.querySelector("#humidity");
-  //   humidity.innerHTML = `Humidity:${response.data.main.humidity}%`;
-  //   document.querySelector("#temperature-description").innerHTML =
-  // response.data.weather[0].main;
-  //   let wind = document.querySelector("#wind");
-  //   wind.innerHTML = `Wind:${response.data.wind.speed} km/h`;
-
-
 let apiKey = "2672779dd04931dc0259289fbe9db665";
 
-let city = "paris";
+let city = "kermanshah";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
